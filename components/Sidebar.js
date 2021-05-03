8ead7b58742f0ba7e0c659c48116c044a07cdcc0
luigi009 <luigifanaro@hotmail.com>
2021-05-03 16:15:06 -0300

    Upload

---
+++ components/Sidebar.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import { Avatar, Button } from '@material-ui/core'
+import ChatIcon from '@material-ui/icons/Chat'
+import MoreVertIcon from '@material-ui/icons/MoreVert'
+import IconButton from '@material-ui/core/IconButton'
+import SearchIcon from '@material-ui/icons/Search'
+
+function Sidebar() {
+    return (
+        <>
+            <div>
+                <header className="flex sticky top-0 bg-white z-[1] justify-between items-center p-5 h-[80px] border-b-2 border-fuchsia-600">
+                    <Avatar className="cursor-pointer opacity-100 hover:opacity-80" />
+                    <div>
+                        <IconButton>
+                            <ChatIcon />
+                        </IconButton>
+                        <IconButton>
+                            <MoreVertIcon />
+                        </IconButton>
+                    </div>
+                </header>
+
+                <div>
+                    <div className="flex items-center p-6 rounded-full">
+                        <SearchIcon />
+                        <input className="outline-none border-none flex-1" placeholder="Search in chats" />
+                    </div>
+                    <div className="p-6 pt-1">
+                        <Button className="w-100">Start a new chat</Button>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Sidebar
